9e7bae5783f010ccca1d66bf258fd775b64b2ed7
Denis Kuznetsov <denis.kuznetsov.kalistos@gmail.com>
2013-11-21 14:34:55 +0400

    1.0-alpha-1

--- od_api.php
+++ od_api.php
@@ -10,10 +10,10 @@ include_once 'classes/od_api_object.inc';
 // Include object abstract classes.
 include_once 'classes/objects/default/od_api_list.inc';
 include_once 'classes/objects/default/od_api_meta.inc';
-// include_once 'classes/objects/default/od_api_publisher.inc';
-// include_once 'classes/objects/default/od_api_source.inc';
-// include_once 'classes/objects/default/od_api_data.inc';
-// include_once 'classes/objects/default/od_api_structure.inc';
+include_once 'classes/objects/default/od_api_publisher.inc';
+include_once 'classes/objects/default/od_api_source.inc';
+include_once 'classes/objects/default/od_api_data.inc';
+include_once 'classes/objects/default/od_api_structure.inc';
 
 // Include parsers.
 include_once 'classes/parsers/od_api_rdfa_parser.inc';
@@ -22,7 +22,7 @@ include_once 'classes/parsers/od_api_csv_parser.inc';
 // Include object classes.
 include_once 'classes/objects/versions/2.3/od_api_list.inc';
 include_once 'classes/objects/versions/2.3/od_api_meta.inc';
-// include_once 'classes/objects/versions/2.3/od_api_publisher.inc';
-// include_once 'classes/objects/versions/2.3/od_api_source.inc';
-// include_once 'classes/objects/versions/2.3/od_api_data.inc';
-// include_once 'classes/objects/versions/2.3/od_api_structure.inc';
+include_once 'classes/objects/versions/2.3/od_api_publisher.inc';
+include_once 'classes/objects/versions/2.3/od_api_source.inc';
+include_once 'classes/objects/versions/2.3/od_api_data.inc';
+include_once 'classes/objects/versions/2.3/od_api_structure.inc';
